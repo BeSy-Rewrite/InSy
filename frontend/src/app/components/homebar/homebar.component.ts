5a9edc498b222140e1e97e960e8504abae1dcb66
Thr0in <xthroin@gmail.com>
2025-10-04 00:10:55 +0200

    Add BeSy HomebarComponent for navigation bar
    Introduces HomebarComponent with HTML, SCSS, TypeScript, and test files. Provides a responsive navigation bar with menu and login indicator, supporting both desktop and mobile layouts.

---
+++ frontend/src/app/components/homebar/homebar.component.ts
@@ -0,0 +1,43 @@
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { Router, RouterModule } from '@angular/router';
+import { LoginComponent } from "../login-indicator/login.component";
+import { NavbarButtonComponent } from '../navbar-button/navbar-button.component';
+
+@Component({
+  selector: 'app-homebar',
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    RouterModule,
+    LoginComponent,
+    NavbarButtonComponent
+  ],
+  templateUrl: './homebar.component.html',
+  styleUrls: ['./homebar.component.scss'],
+})
+export class HomebarComponent {
+  isMobileMenuOpen = signal(false);
+  activeMenuItem = signal(0);
+
+  links = [
+    { name: 'Startseite', path: '/home' },
+    { name: 'Inventar', path: '/inventory' },
+    { name: 'Bestellungen', path: '/orders' },
+    { name: 'Neue Inventarisierung', path: '/new' },
+    { name: 'Neue Erweiterung', path: '/new-extension' },
+    { name: 'Import', path: '/import' }
+  ];
+
+  constructor(public readonly router: Router) {
+    router.events.subscribe(() => {
+      this.activeMenuItem.set(
+        this.links.findIndex(link => link.path === `/${router.url.split('/')[1]}`)
+      );
+    });
+  }
+
+}
